229423ae41c41c8e069ad4013fa8dac33800c666
jesus <jesus.quinteros.c@gmail.com>
2025-07-06 21:16:33 -0500

    modificando la vista general

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,11 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { useRoute } from 'vue-router'
+import Sidebar from '@/components/Sidebar.vue'
+
+const route = useRoute()
+const hideSidebarRoutes = ['/login']
 </script>
 
 <template>
-  <RouterView />
+  <div class="layout">
+    <!-- Sidebar solo si no estás en login -->
+    <Sidebar v-if="!hideSidebarRoutes.includes(route.path)" />
+
+    <!-- Contenido de la vista -->
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style>
+.layout {
+  display: flex;
+}
 
+.content {
+  padding: 0rem;
+  width: 100%;
+}
 </style>
