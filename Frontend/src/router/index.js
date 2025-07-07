9841e348dffa98c70dfbc6013f7ce8e042b3ffb7
jesus <jesus.quinteros.c@gmail.com>
2025-07-06 21:18:30 -0500

    routeando la vista de creategroup

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
+import CreategroupView from '@/views/CreategroupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-        path: '/login',
-        name: 'login',
-        component: LoginView
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/creategroup',
+      name: 'creategroup',
+      component: CreategroupView
     },
   ],
 })
