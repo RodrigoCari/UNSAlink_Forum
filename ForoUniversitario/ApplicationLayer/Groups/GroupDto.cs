7d2f3ee9144460e5a107fcaa1c0cd586425ab53c
Rodrigo Cari <jcarial@unsa.edu.pe>
2025-07-26 20:01:08 -0500

    Extend GroupDto to include LatestPost for latest post display in home view

--- ForoUniversitario/ApplicationLayer/Groups/GroupDto.cs
+++ ForoUniversitario/ApplicationLayer/Groups/GroupDto.cs
@@ -1,4 +1,6 @@
-﻿namespace ForoUniversitario.ApplicationLayer.Groups;
+﻿using ForoUniversitario.ApplicationLayer.Posts;
+
+namespace ForoUniversitario.ApplicationLayer.Groups;
 
 public class GroupDto
 {
@@ -6,4 +8,5 @@ public class GroupDto
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public Guid AdminId { get; set; }
+    public PostDto? LatestPost { get; set; }
 }
